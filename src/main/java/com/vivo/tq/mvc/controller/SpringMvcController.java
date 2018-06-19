48214d5735937bb4bc782d4fdf34247ddc40f488
汤旗 <tangqihlw@bbktel.com>
2018-06-19 15:59:56 +0800

    代码优化

--- src/main/java/com/vivo/tq/mvc/controller/SpringMvcController.java
+++ src/main/java/com/vivo/tq/mvc/controller/SpringMvcController.java
@@ -1,30 +1,22 @@
 package com.vivo.tq.mvc.controller;
 
-import com.vivo.tq.mvc.model.FormatModel;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.RequestContext;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
 
 /**
- * 类描述：
+ * 类描述：Spring MVC 测试
  *
  * @author 汤旗
  * @date 2018-06-19
  */
-@Controller
+@RestController
 @RequestMapping(value = "/mvc")
 public class SpringMvcController {
 
     @RequestMapping(value = "/test", method = {RequestMethod.GET})
-    public String test(@RequestParam(value = "id",required = true) Integer id) {
-
-        return "globaltest";
+    public String test(@RequestParam(value = "id", required = true) Integer id) {
+        return "mvctest";
     }
 }
